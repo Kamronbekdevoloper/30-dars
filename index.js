cdc12679860cd7de03f52975603750b59dfbf8c6
Kamronbekdevoloper <kamronbekdevoloper@gmail.com>
2025-01-11 11:18:52 +0500

    html va js yaratdim

---
+++ index.js
@@ -0,0 +1,89 @@
+// let hasPassport = true;
+// let halfsince = false;
+
+// console.log(hasPassport);
+// let jack = 25;
+// let jak = 30;
+
+// let jet = "true";
+// let dm = 5;
+// let sm = dm * 10;
+// console.log(sm);
+
+// let gr = 3278;
+// let kg = gr / 1000;
+// console.log(kg);
+
+// let l = 56;
+// let ml = l * 1000;
+// console.log(ml);
+
+// let hasKeyBoss = true;
+// if (hasKeyBoss == true) {
+//   console.log("office open");
+// } else {
+//   console.log("office close");
+// // }
+// let num = 78;
+// Result = num % 2;
+
+// if (Result === 0) {
+//   console.log("Juft son");
+// } else {
+//   console.log("Toq son");
+// }
+
+// let num2 = 0;
+
+// if (num2 > 0) {
+//   console.log("musbat son");
+// } else if (num2 < 0) {
+//   console.log("manfiy son");
+// } else {
+//   console.log("bu son nol ");
+// }
+
+// let weekday = 3;
+// // none = weekday > 7;
+
+// if (weekday === 1) {
+//   console.log("Dushanba");
+//   // } else if (none > weekday) {
+//   //   console.log("hafta yoq");
+// } else if (weekday === 2) {
+//   console.log("seshanba");
+// } else if (weekday === 3) {
+//   console.log("chorshanba");
+// } else if (weekday === 4) {
+//   console.log("payshanba");
+// } else if (weekday === 5) {
+//   console.log("juma");
+// } else if (weekday === 6) {
+//   console.log("shanba");
+// } else if (weekday === 7) {
+//   console.log("yakshanba");
+// } else {
+//   console.log("bu kun yoq");
+// }
+
+// let season = "yoz";
+
+// if (season === "qish") {
+//   console.log("dekabr yanvar fevral");
+// } else if (season === "bahor") {
+//   console.log("mart aaprel may");
+// } else if (season === "kuz") {
+//   console.log("sentabr oktabr noyabr");
+// } else if (season === "yoz") {
+//   console.log("iyun iyul avgust");
+// } else {
+//   console.log("hato malumot");
+// }
+let soat = 19;
+if (soat > 18) {
+  console.log("good morning");
+} else if (soat < 18) {
+  console.log("good evening");
+} else {
+  console.log("uhla");
+}
